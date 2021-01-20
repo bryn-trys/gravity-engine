fce46e9858379cf331776f6955ad1248ffdc1cbd
Bryn <63167432+bryn-trys@users.noreply.github.com>
2021-01-19 18:30:07 -0800

    Create main.cs

---
+++ main.cs
@@ -0,0 +1,17 @@
+#main {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  margin: auto;
+}
+
+#canvas {
+  border: rgba(100, 150, 100, 0, 7) 5px double;
+  border-radius: 5%;
+  background-color: black;
+  position: absolute;
+  left: 50%;
+  margin-left: -250px;
+  top: 50%;
+  margin-top: -100px;
+}
